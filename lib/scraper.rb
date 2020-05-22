3c628e45f825f6c4394e418d18fb1225c86637fc
Vianka Rivera <viankalrivera@gmail.com>
2020-05-22 13:16:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  
+   def get_page
+     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+     binding.pry  
+    
+  end 
+  
+  def make_courses
+  
+  end
   
   def print_courses
     self.make_courses
@@ -17,6 +28,8 @@ class Scraper
   end
   
 end
+
+Scraper.new.get_page
 
 
 
