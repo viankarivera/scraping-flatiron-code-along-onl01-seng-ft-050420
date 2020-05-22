122bad5d9f851dee5564e971bd82d221e6abf9ea
Vianka Rivera <viankalrivera@gmail.com>
2020-05-22 13:23:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,6 +14,8 @@ class Scraper
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
+      
+      binding.pry
     end
   
   def make_courses
